bb796d6a88e597b7ce76b5dd2fe8c5e03a041a51
Nethelbert <Gilblasse@gmail.com>
2020-11-27 14:14:30 -0500

    init app w. header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+// Step1 
+//   * Navbar
+//   * Hero 
+//     ** animation
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <header>
+        <nav id="navigation-bar">
+          <span id="logo">Events2Luv</span>
+          <span id="reservations">Reservations</span>
+          <div className="hamburger-menu-wrapper">
+            <span className="hamburger-menu"></span>
+          </div>
+        </nav>
+
+        <section>
+
+        </section>
       </header>
+
     </div>
   );
 }
